4872208309badec99bac474097256fbc8469ca56
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-08-27 13:37:41 +1000

    auth production fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
         mongodb_username: 'gideon100',
         mongodb_password: '140993',
         mongodb_db: 'eventspage-newsletter',
-        NEXTAUTH_URL: 'https://localhost:3000',
+        // NEXTAUTH_URL: 'https://localhost:3000',
+        NEXTAUTH_URL: 'https://invoiceapp234.netlify.app/',
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
     },
 }
